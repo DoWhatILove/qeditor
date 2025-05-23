5e48fc5ac60973f491a6805e1cb1cd74663fc6b4
David Chen (MSAI) <davch@microsoft.com>
2025-05-23 10:41:19 -0500

    add the src to sys path for testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
-import pytest
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
 import tempfile
 import json
 from flask import Flask
@@ -146,4 +149,3 @@ def test_get_file_paths(app):
     filename, filepath = get_file_paths('test', 'MODIFIED_FOLDER', app, session_id)
     assert filename == 'test_modified.tsv'
     assert os.path.exists(os.path.dirname(filepath))
-    
